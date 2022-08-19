bbf0bf4cdb4758554aceba982d0283958764dac7
abdulhamiid <adioabdulhamid@yahoo.com>
2022-08-19 12:39:52 +0100

    Adjust code as requested by reviewer

--- mylist.rb
+++ mylist.rb
@@ -6,8 +6,12 @@ class MyList
     @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
   end
 
   def filter(&block)
